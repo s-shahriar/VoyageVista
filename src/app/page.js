bfb1c7e679b98782b71c0e48bd03f93d69fc0f09
Syed/TECH/Syed Shahriar (Email: syed.shahriar@robi.com.bd) <syed.shahriar@robi.com.bd>
2024-09-19 17:16:34 +0600

    other project int

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import AnimalCategorization from "./components/AnimalCategorization";
 import Banner from "./components/Homepage/Banner";
 
 export default function Home() {
   return (
     <>
     <Banner />
+    <AnimalCategorization></AnimalCategorization>
     </>
   );
 }
